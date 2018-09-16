490e7264e840d598ab4bd6f923ff58d782cdc9ec
lukejsnyder95 <43047226+lukejsnyder95@users.noreply.github.com>
2018-09-16 17:54:44 -0400

    Add files via upload

---
+++ MaryPoppinsTote.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+class MarryPoppinsTote{
+
+public:
+int toteSize;
+//MarryPoppinsTote int *tote;
+int tote[];
+
+private:
+
+MarryPoppinsTote(){
+  //Initialize
+
+  //check to see if array tote has any objects
+  if(tote == NULL){
+  tote = new int tote[1];
+  }
+  else{
+  //int newToteSize = toteSize;
+    MarryPoppinsTote* newTote = new MarryPoppinsTote[toteSize + 1];
+  }
+
+//Copy Constructor: Coppying data from original Tote to newTote
+MarryPoppinsTote(const MarryPoppinsTote & other){
+  //Inizialize
+  this.tote = int[other.toteSize];
+  for (index i = 0; i < toteSize; i++) {
+      newTote[i] = tote[i];
+  }
+//  this.newTote = other.tote;
+
+}
+
+//Deconstructor to delete orignal tote array
+~MarryPoppinsTote(){
+  delete[] tote;
+  }
+
+void place(int item){
+
+  }
+
+int search(int item){
+  for(int i = 0; i<tote.size )
+
+  }
+
+int reach(){
+
+
+
+}
+
+MarryPoppinsTote& operator=(const MarryPoppinsTote & other){
+
+  }
+
+friend ostream& operator<<(ostream& os, const MarryPoppinsTote & me){
+
+}
+
+
+
+}
