b785f74b78cd50e55a61fb9487f18c42a5006cab
lukejsnyder95 <43047226+lukejsnyder95@users.noreply.github.com>
2018-09-16 18:07:21 -0400

    Update MaryPoppinsTote.cpp

--- MaryPoppinsTote.cpp
+++ MaryPoppinsTote.cpp
@@ -47,12 +47,11 @@ int search(int item){
   int count = 0;
   for(int i = 0; i<tote.size; i++ ){
       if(item == tote[i]){
-        count = count +1;
-      }else{
-        return 0;
+        count = count +1;  
       }
 
     }
+    return count;
 
   }
 
